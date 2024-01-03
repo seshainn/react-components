c07eea707c2843824cc16c3e7dd9f17e18be5b48
seshu <seshu.giri@gmail.com>
2024-01-03 21:08:59 +0530

    Add: Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
 import { useState } from 'react'
 import Accordion from './components/Accordion'
 import Tabs from './components/Tabs'
+import Modal from './components/Modal'
 
 function App() {
   const [showAccordion, setShowAccordion] = useState(false)
   const [showTabs, setShowTabs] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <div className='flex min-h-screen'>
       <div className='w-1/4 p-4 flex flex-col gap-4 border border-slate-400 rounded-xl m-1'>
         <button
           type='button'
-          className='w-full border-2 border-blue-500 bg-blue-500 px-4 py-2 rounded-md font-semibold tracking-wider shadow-lg backdrop-blur hover:bg-blue-900 hover:border-blue-700 hover:text-blue-300'
+          className='w-full border-2 border-slate-400 bg-slate-400 px-4 py-2 rounded-md font-semibold tracking-wider shadow-lg backdrop-blur hover:bg-slate-500 hover:border-slate-500 hover:text-slate-100'
           onClick={() => {
             setShowAccordion(!showAccordion)
             setShowTabs(false)
+            setShowModal(false)
           }}
         >
           Accordion
         </button>
         <button
           type='button'
-          className='w-full border-2 border-blue-500 bg-blue-500 px-4 py-2 rounded-md font-semibold tracking-wider shadow-lg backdrop-blur hover:bg-blue-900 hover:border-blue-700 hover:text-blue-300'
+          className='w-full border-2 border-slate-400 bg-slate-400 px-4 py-2 rounded-md font-semibold tracking-wider shadow-lg backdrop-blur hover:bg-slate-500 hover:border-slate-500 hover:text-slate-100'
           onClick={() => {
             setShowAccordion(false)
             setShowTabs(!showTabs)
+            setShowModal(false)
           }}
         >
           Tabs
+        </button>
+        <button
+          type='button'
+          className='w-full border-2 border-slate-400 bg-slate-400 px-4 py-2 rounded-md font-semibold tracking-wider shadow-lg backdrop-blur hover:bg-slate-500 hover:border-slate-500 hover:text-slate-100'
+          onClick={() => {
+            setShowAccordion(false)
+            setShowTabs(false)
+            setShowModal(true)
+          }}
+        >
+          Modal
         </button>
       </div>
 
@@ -63,6 +79,15 @@ function App() {
           </div>
         )}
         {showTabs && <Tabs />}
+        {showModal && (
+          <Modal
+            onClose={() => {
+              setShowModal(false)
+            }}
+          >
+            <h1 className='font-bold text-lg'>Content goes here</h1>
+          </Modal>
+        )}
       </div>
     </div>
   )
---
+++ src/components/Modal.tsx
@@ -0,0 +1,73 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+const Modal = ({
+  onClose,
+  children,
+}: {
+  onClose: () => void
+  children: React.ReactNode
+}) => {
+  const dialogRef = useRef<HTMLDivElement>(null)
+  const handleClose = useCallback(() => {
+    onClose()
+  }, [onClose])
+  const handleEscapeKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        handleClose()
+      }
+    },
+    [handleClose]
+  )
+
+  const handleOutsideClick = useCallback(
+    (event: MouseEvent) => {
+      if (
+        dialogRef.current &&
+        !dialogRef.current.contains(event.target as Node)
+      ) {
+        handleClose()
+      }
+    },
+    [handleClose]
+  )
+  useEffect(() => {
+    document.addEventListener('keydown', handleEscapeKeyPress)
+    document.addEventListener('mousedown', handleOutsideClick)
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKeyPress)
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [handleEscapeKeyPress, handleOutsideClick])
+
+  return (
+    <div className='w-[100%] h-[100%] relative'>
+      <div
+        ref={dialogRef}
+        className='absolute top-20 bottom-20 left-20 right-20 border border-slate-200 rounded-xl shadow-lg backdrop-blur bg-white z-20 flex flex-col items-center justify-center gap-4'
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          stroke-width='2'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          className='lucide lucide-x absolute top-4 right-4 cursor-pointer'
+          onClick={() => {
+            handleClose()
+          }}
+        >
+          <path d='M18 6 6 18' />
+          <path d='m6 6 12 12' />
+        </svg>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Modal
